ba7b728efc2254f38934c79ef063eedd1a4fbd99
David Sukhashvili <davidsukhashvili@MacBook-Pro-David.local>
2025-02-26 23:37:18 +0300

    feature0: add parse all links from site and write it to file

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,49 @@
-    use reqwest;
-    use scraper::{Html, Selector};
-    use std::error::Error;
-    use std::io;
+use reqwest;
+use scraper::{ Html, Selector };
+use std::error::Error;
+use std::io;
+use std::fs::File;
+use std::io::Write;
 
+async fn fetch_html(url: &str) -> Result<String, Box<dyn Error>> {
+    let response = reqwest::get(url).await?.text().await?;
+    Ok(response)
+}
 
-    async fn fetch_html(url: &str) -> Result<String, Box<dyn Error>> {
-        let response = reqwest::get(url).await?.text().await?;
-        Ok(response)
-    }
-    
+fn extract_links(html: &str) -> Vec<String> {
+    let document = Html::parse_document(html);
+    let selector = Selector::parse("a").unwrap();
 
+    document
+        .select(&selector)
+        .filter_map(|element| element.value().attr("href"))
+        .map(|href| href.to_string())
+        .collect()
+}
 
-    fn extract_links(html: &str) -> Vec<String> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let mut file = File::create("links.txt")?;
 
-        
-        let document = Html::parse_document(html);
-        let selector = Selector::parse("a").unwrap();
-       
-        document
-            .select(&selector)
-            .filter_map(|element| element.value().attr("href"))
-            .map(|href| href.to_string())
-            .collect()
-    }
-    
-    #[tokio::main]
-    async fn main() {
-        println!("Введите URL для парсинга:");
-        let mut url = String::new();
-        io::stdin().read_line(&mut url).expect("Ошибка ввода URL");
-        let url = url.trim();
+    println!("Введите URL для парсинга:");
+    let mut url = String::new();
+    io::stdin().read_line(&mut url)?;
+    let url = url.trim();
 
-
-        match fetch_html(url).await {
-            Ok(html) => {
-                let links = extract_links(&html);
-                println!("Найденные ссылки:");
-                for link in links {
-                    println!("{}", link);
-                }
+    match fetch_html(url).await {
+        Ok(html) => {
+            let links = extract_links(&html);
+            println!("Найденные ссылки:");
+            let mut my_string = String::new();
+            for link in links {
+                println!("{}", link);
+                my_string.push_str(&format!("{}\n", link));
             }
-            Err(e) => eprintln!("Ошибка: {}", e),
+
+            file.write_all(my_string.as_bytes())?;
         }
+        Err(e) => eprintln!("Ошибка: {}", e),
     }
+
+    Ok(())
+}
